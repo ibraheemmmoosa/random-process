250ab3c642b7bf1ee95e16d838e5e3cdd8ebcc06
ibraheemmmoosa <45825930+ibraheemmmoosa@users.noreply.github.com>
2021-01-05 11:37:27 +0600

    Random walk with addition.

---
+++ random-walk-with-addition.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# p is probability of rise
+m = 100
+p = 0.5
+r = np.array([1.0, -1.0])
+n = 100000
+
+def func(p, r, n):
+    P = np.random.rand(n)
+    P = (P < p).astype(int)
+    D = 2 * P - 1
+
+    zz = np.sum(np.convolve(D, [-1, -1], mode='valid') == 2)
+    oz = np.sum(np.convolve(D, [1, -1], mode='valid') == 2)
+    zo = np.sum(np.convolve(D, [-1, 1], mode='valid') == 2)
+    oo = np.sum(np.convolve(D, [1, 1], mode='valid') == 2)
+    tot = zz + oz + zo + oo
+
+    pos = np.cumsum(r[P])
+
+    return pos
+
+poses = [func(p, r, n) for i in range(m)]
+pos = sum(poses) / m
+
+for i in range(m):
+    plt.plot(poses[i])
+
+plt.plot(pos, linewidth=2.0, c='black')
+plt.show()
