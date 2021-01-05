e224f431608a6eebcd2de8314d5b01d2bd3801d3
ibraheemmmoosa <45825930+ibraheemmmoosa@users.noreply.github.com>
2021-01-05 13:24:15 +0600

    Combine multiplication and addition into one model.

--- bernoulli-random-walk.py
+++ bernoulli-random-walk.py
@@ -6,15 +6,17 @@ rng = default_rng()
 # p is probability of rise
 m = 100
 p = 0.5
-r = np.array([1.0 + 1.1e-3, 1.0 - 1e-3])
+r = np.log(np.array([1.0 + 1.1e-3, 1.0 - 1e-3]))
+# r = np.array([1.0, -1.0])
 n = 100000
 
-def func(p, r, n):
+def cs_rv(p, r, n):
     P = rng.choice(r, size=n, p=[p, 1 - p], replace=True)
-    pos = np.cumprod(P)
+    pos = np.cumsum(P)
     return pos
 
-poses = [func(p, r, n) for i in range(m)]
+poses = [cs_rv(p, r, n) for i in range(m)]
+poses = list(map(np.exp, poses))
 pos = sum(poses) / m
 
 subsample_every = 100
